f1c69fdf8f69317d2c3d3a86b2bb410368979771
Joseph Hall <jhall0188@myduneland.org>
2021-02-25 08:55:03 -0600

    Added first three getter methods

---
+++ Loan.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class Loan here.
+ *
+ * @Ben Shade, Alex Harmeling, Joseph Hall
+ * @version (a version number or a date)
+ */
+public class Loan
+{
+    // instance variables - replace the example below with your own
+    private int ID;
+    private double loanAmount;
+    private String country;
+    private int daysToFund;
+    private int numLenders;
+    
+   public Loan(int ID, double loanAmount, String country, int daysToFund, 
+            int numLenders) {
+                this.ID = ID;
+                this.loanAmount = loanAmount;
+                this.country = country;
+                this.daysToFund = daysToFund;
+                this.numLenders = numLenders;
+   }
+   
+   public int getID()
+   {
+       return this.ID;
+   }
+   
+   public double getLoanAmount()
+   {
+       return this.loanAmount;
+   }
+   
+   public String getCountry()
+   {
+       return this.country;
+   }
+   
+    
+}
