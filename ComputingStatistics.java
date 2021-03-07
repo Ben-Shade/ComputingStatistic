a5189c0ce19501e26c4c9bc00a170e16fea9bbb8
Ben Shade <bshade0211@myduneland.org>
2021-03-06 23:03:30 -0800

    gggg

--- ComputingStatistics.java
+++ ComputingStatistics.java
@@ -33,6 +33,7 @@ public class ComputingStatistics {
       }
       return amount;
    }
+   //Calculates and returns the average amount funded from all the loans.
    public double avgLoan()
    {
       double average = 0.0;
@@ -46,8 +47,8 @@ public class ComputingStatistics {
       }
       return average/counter;
    }
-
-    public double largestLoan()
+   //Finds the largest loan amount from all of the loans.
+   public double largestLoan()
    {
        double largest = 0.0;
        Loan loan;
@@ -61,6 +62,7 @@ public class ComputingStatistics {
        }
        return largest;
    }
+<<<<<<< HEAD
    public double smallestLoan(){
        double smallest = 0.0;
        Loan loan;
@@ -87,4 +89,41 @@ public class ComputingStatistics {
         }
         return country;
     }
+=======
+   //Calculates the average amount of days to fund all of the loans.
+   public double avgDaysToFund()
+   {
+       Loan loan;
+       double average = 0.0;
+       int counter = 0;
+       for (int i = 0; i< data.size(); i++)
+       {
+           loan = data.get(i);
+           average = average + loan.getDaysToFund();
+           counter++;
+       }
+       return average/counter;
+   }
+   public double largestLoanC(String Country)
+   {
+       Loan loan;
+       double largest = 0.0;
+       for (int i = 0; i < data.size(); i++)
+       {
+           loan = data.get(i);
+           if(loan.getCountry().equals(Country))
+           {
+               if(loan.getLoanAmount() > largest)
+               {
+                   largest = loan.getLoanAmount();
+               }
+           }
+       }
+       return largest;
+   }
+   
+
+
+
+>>>>>>> 042c0eac8641ee6e6fb0e937c0bcc52dcf57f463
 }
